c59dd56224e451919c1f5289ef985068e5d550e2
Alan <alanrodrigo.mg@gmail.com>
2025-07-17 11:30:21 -0400

    Mejorando los links con logica y tailwind y routeIs para ver donde estamos

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <script src="{{ asset('js/scripts.js') }}"></script> --}}
     <title>{{ $metaTitle ?? 'Default title' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default description' }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     {{-- @include('partials.navigation') --}}
--- resources/views/components/partials/navigation.blade.php
+++ resources/views/components/partials/navigation.blade.php
@@ -1,6 +1,12 @@
-<ul>
-    <li><a href="{{ route('welcome') }}">Home</a></li>
-    <li><a href="{{ route('posts.index') }}">Blog</a></li>
-    <li><a href="{{ route('about') }}">Nosotros</a></li>
-    <li><a href="{{ route('contact') }}">Contacto</a></li>
-</ul>
+{{-- @dump(request()->route()) --}}
+<nav>
+    <ul>
+        <li><a href="{{ route('welcome') }}" class="{{ request()->routeIs('welcome') ? 'text-green-400' : '' }}">Home</a></li>
+        <li><a href="{{ route('posts.index') }}" class="{{ request()->routeIs('posts.*') ? 'text-green-400' : '' }}">Blog</a></li>
+        <li><a href="{{ route('about') }}" class="{{ request()->routeIs('about') ? 'text-green-400' : '' }}">Nosotros</a></li>
+        <li><a href="{{ route('contact') }}" class="{{ request()->routeIs('contact') ? 'text-green-400' : '' }}">Contacto</a></li>
+        {{-- <x-nav-link href="{{ route('welcome') }}" :active="request()->routeIs('welcome')">
+            Home
+        </x-nav-link> --}}
+    </ul>
+</nav>
