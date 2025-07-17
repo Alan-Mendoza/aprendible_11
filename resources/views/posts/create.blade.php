b1ce40d73aa8d6ddc0939ea18c3ee7f51f35d987
Alan <alanrodrigo.mg@gmail.com>
2025-07-17 09:33:55 -0400

    Aprendiendo a utilizar Lang de laravel correctamente para las traducciones

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,16 +1,16 @@
 <x-layout meta-title="Create New Post" meta-description="Create a new blog post">
-    <h1>Create New Post</h1>
+    <h1>{{ __('Create New Post') }}</h1>
     {{-- @dump($errors->all()) --}}
     <form action="{{ route('posts.store') }}" method="POST">
         @csrf
-        <label for="title">Title</label><br>
+        <label for="title">{{ __('Title') }}</label><br>
         <input type="text" name="title" id="title" value="{{ old('title') }}"><br>
         @error('title') <span style="color: red;">{{ $message }}</span> @enderror
         <br>
-        <label for="body">Body</label><br>
+        <label for="body">{{ __('Body') }}</label><br>
         <textarea name="body" id="body">{{ old('body') }}</textarea><br>
         @error('body') <span style="color: red;">{{ $message }}</span> @enderror
         <br>
-        <button type="submit">Create Post</button>
+        <button type="submit">{{ __('Send') }}</button>
     </form>
 </x-layout>
